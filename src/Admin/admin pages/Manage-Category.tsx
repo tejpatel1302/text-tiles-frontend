335e6d9d07dc3fb66e251b972bc08803e15f3f14
Ravindra Valand ᴿⱽ <thekingjarvis792@gmail.com>
2024-04-12 21:50:45 +0530

    Refactor Manage-Category component and update category management UI

--- src/Admin/admin pages/Manage-Category.tsx
+++ src/Admin/admin pages/Manage-Category.tsx
@@ -3,20 +3,16 @@ import { DataTable } from "@/components/common/Data-table";
 import { Category, columns } from "@/utils/category-column";
 
 const ManageCategory = () => {
-  const data: Category[] = category.map((item: any) => ({
+  const data: Category[] = category.map((item) => ({
     categoryID: item.categoryID,
     categoryName: item.categoryName,
     categoryDescription: item.categoryDescription,
     image: item.image,
-    
-
   }));
-
-
 
   return (
     <div className="bg-white">
-      <div className="text-3xl font-bold">Order Details:</div>
+      <div className="text-3xl font-bold">Category Management:</div>
       <div>
         <DataTable columns={columns} data={data} />
       </div>
